b3a1ab10f6fe4b853ee506b1ddb1bf8affb407b6
Lucas-DVS <lucas.metal151@gmail.com>
2023-08-21 23:46:04 -0300

    Commit 0

---
+++ main.py
@@ -0,0 +1,6 @@
+from website import create_app
+
+app = create_app()
+
+if __name__ == '__main__': # Significa que só se rodarmos o file __name__, não se importarmos o mesmo, o programa executará o app.run()
+    app.run(debug=True)
